35a1f854e7a2ec1e75f6ffaacb733e3f8f5fe4e2
LilitAdoyan <lilith.adoyan@gmail.com>
2021-12-14 23:56:35 +0400

    slides

--- js/script.js
+++ js/script.js
@@ -187,36 +187,43 @@ const getResource = async (url) =>{
     return await res.json();
 };
 
-// getResource('http://localhost:3004/menu')
+// getResource('http://localhost:3000/menu')
 //     .then(data=>{
 //         data.forEach(({img, altimg, title, descr, price})=>{
 //             new MenuCard(img, altimg, title, descr, price, '.menu .container').render();
 //         });
 //     });
 
-getResource('http://localhost:3004/menu')
-    .then(data => createCard(data));
-
-    function createCard(data){
-        data.forEach(({img, altimg, title, descr, price})=>{
-            const element=document.createElement('div');
-
-            element.classList.add('menu__item');
-
-            element.innerHTML=`
-            <img src=${img} alt=${altimg}>
-            <h3 class="menu__item-subtitle">${title}</h3>
-            <div class="menu__item-descr">${descr}</div>
-            <div class="menu__item-divider"></div>
-            <div class="menu__item-price">
-                <div class="menu__item-cost">Цена:</div>
-                <div class="menu__item-total"><span>${price}</span> грн/день</div>
-            </div>
-            `;
-
-            document.querySelector('.menu .container').append(element);
+axios.get('http://localhost:3000/menu')
+    .then(data=>{
+        data.data.forEach(({img, altimg, title, descr, price})=>{
+        new MenuCard(img, altimg, title, descr, price, '.menu .container').render();
         });
-    }
+
+
+// getResource('http://localhost:3000/menu')
+//     .then(data => createCard(data));
+
+    // function createCard(data){
+    //     data.forEach(({img, altimg, title, descr, price})=>{
+    //         const element=document.createElement('div');
+
+    //         element.classList.add('menu__item');
+
+    //         element.innerHTML=`
+    //         <img src=${img} alt=${altimg}>
+    //         <h3 class="menu__item-subtitle">${title}</h3>
+    //         <div class="menu__item-descr">${descr}</div>
+    //         <div class="menu__item-divider"></div>
+    //         <div class="menu__item-price">
+    //             <div class="menu__item-cost">Цена:</div>
+    //             <div class="menu__item-total"><span>${price}</span> грн/день</div>
+    //         </div>
+    //         `;
+
+    //         document.querySelector('.menu .container').append(element);
+    //     });
+    // }
 //FORMS
 
 
@@ -259,7 +266,7 @@ function bindPostData(form){
         
       const json = JSON.stringify(Object.fromEntries(formData.entries()));
 
-        postData('http://localhost:3004/requests', json)
+        postData('http://localhost:3000/requests', json)
         .then(data=>{
             console.log(data);
             showThanksModal(message.success);
@@ -298,7 +305,114 @@ function bindPostData(form){
 
 });
 
+//slider
+
+    const slides=document.querySelectorAll('.offer__slide'),
+        prev = document.querySelector('.offer__slider-prev'),
+        next = document.querySelector('.offer__slider-next');
+        total=document.querySelector('#total'),
+        current=document.querySelector('#current'),
+        slidesWrapper=document.querySelector('.offer__slider-wrapper'),
+        slidesField=document.querySelector('.offer__slider-inner'),
+        width = window.getComputedStyle(slidesWrapper).width;
+        let slideIndex=1;
+        let offset=0;
+
+        if(slides.length<10){
+            total.textContent = `0${slides.length}`;
+            current.textContent=`0${slideIndex}`;
+        } else{
+            total.textContent = slides.length;
+            current.textContent=slideIndex;
+        }
 
 
+        slidesField.style.width=100*slides.length+'%';
+        slidesField.style.display = 'flex';
+
+        slidesWrapper.style.overflow='hidden';
+        slidesField.style.transition='0.5s all';
+        slides.forEach(slides => {
+            slides.style.width=width;
+        });
+
+        next.addEventListener('click',()=>{
+            if(offset==+width.slice(0,width.length-2) * (slides.length-1)){
+                offset=0;
+            } else {
+                offset += +width.slice(0,width.length-2);
+            }
+
+            slidesField.style.transform=`translate(-${offset}px)`;
+            if (slideIndex==slides.length) {
+                slideIndex=1;
+            } else {
+                slideIndex++;
+            }
+
+            if (slides.length<10){
+                current.textContent=`0${slideIndex}`;
+            } else {
+                current.textContent=slideIndex;
+            }
+        
+        });
 
 
+        prev.addEventListener('click',()=>{
+            if(offset==0){
+                offset = +width.slice(0,width.length-2) * (slides.length-1)
+            } else {
+                offset -= +width.slice(0,width.length-2);
+            }
+
+            slidesField.style.transform=`translate(-${offset}px)`;
+            if (slideIndex==1) {
+                slideIndex=slides.length;
+            } else {
+                slideIndex--;
+            }
+            
+            if (slides.length<10){
+                current.textContent=`0${slideIndex}`;
+            } else {
+                current.textContent=slideIndex;
+            }
+        });
+    // showSlides(slides);
+
+    // if(slides.length<10){
+    //     total.textContent = `0${slides.length}`;
+    // } else{
+    //     total.textContent = slides.length;
+    // }
+
+    // function showSlides(n){
+    //     if(n>slides.length){
+    //         slideIndex=1; 
+    //     }
+
+    //     if (n<1){
+    //         slideIndex=slides.length;
+    //     }
+    //     slides.forEach(item=>item.style.display='none');
+
+    //     slides[slideIndex-1].style.display='block';
+
+    //     if(slides.length<10){
+    //         current.textContent = `0${slideIndex}`;
+    //     } else{
+    //         current.textContent = slideIndex;
+    //     }
+    // }
+
+    // function plusSlides(n){
+    //     showSlides(slideIndex += n);
+    // }
+    // prev.addEventListener('click', ()=>{
+    //     plusSlides(-1);
+    // });
+    // next.addEventListener('click', ()=>{
+    //     plusSlides(1);
+    // });
+});
